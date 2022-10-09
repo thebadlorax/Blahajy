7b177b84c2b29484fe4a55171f3defa4d9a76f9d
thebadlorax <94083313+thebadlorax@users.noreply.github.com>
2022-10-08 22:02:24 -0400

    Added base Blahajy with no combat

---
+++ zonemap.py
@@ -0,0 +1,10 @@
+zonemap = {
+    "00": {
+        "ZONENAME": "Home",
+        "ZONEDESC": "Your own lovely home!",
+        "NORTH": False,
+        "SOUTH": False,
+        "EAST": False,
+        "WEST": False
+    }
+}
