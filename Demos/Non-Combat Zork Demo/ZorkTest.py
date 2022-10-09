ba9e36d7425fba47b2ae9b293eb0e6ebb42e106e
thebadlorax <94083313+thebadlorax@users.noreply.github.com>
2022-10-08 22:28:43 -0400

    Added the actual demo

---
+++ Demos/Non-Combat Zork Demo/ZorkTest.py
@@ -0,0 +1,44 @@
+import Blahajy
+
+
+def main():
+    Blahajy.clearscreen()
+    Blahajy.update()
+    Blahajy.printscenario()
+    Blahajy.printnarrator()
+    playerinput = str(input(": "))
+
+    if playerinput == 'north':
+        if Blahajy.north():
+            Blahajy.changey(1)
+            main()
+        else:
+            Blahajy.updatenarrator("Walking that way would be impossible")
+            main()
+    elif playerinput == 'south':
+        if Blahajy.south():
+            Blahajy.changey(-1)
+            main()
+        else:
+            Blahajy.updatenarrator("Walking that way would be impossible")
+            main()
+    elif playerinput == 'east':
+        if Blahajy.east():
+            Blahajy.changex(1)
+            main()
+        else:
+            Blahajy.updatenarrator("Walking that way would be impossible")
+            main()
+    elif playerinput == 'west':
+        if Blahajy.west():
+            Blahajy.changex(-1)
+            main()
+        else:
+            Blahajy.updatenarrator("Walking that way would be impossible")
+            main()
+    else:
+        Blahajy.updatenarrator("I do not understand")
+        main()
+
+
+main()
---
+++ Demos/Non-Combat Zork Demo/zonemap.py
@@ -0,0 +1,50 @@
+zonemap = {
+    "00": {
+        "ZONENAME": "Home",
+        "ZONEDESC": "Your own lovely home, with a path to the north. and to the west",
+        "NORTH": True,
+        "SOUTH": False,
+        "EAST": False,
+        "WEST": False
+    },
+    "01": {
+        "ZONENAME": "Path to the north",
+        "ZONEDESC": "A winding red brick road, leading to some kind of village, according to the signs",
+        "NORTH": True,
+        "SOUTH": True,
+        "EAST": False,
+        "WEST": False
+    },
+    "02": {
+        "ZONENAME": "Path to the north",
+        "ZONEDESC": "You can see the smoke rising in the distance, and you hope it to be the chimneys of the houses in the village.",
+        "NORTH": True,
+        "SOUTH": True,
+        "EAST": False,
+        "WEST": False
+    },
+    "03": {
+        "ZONENAME": "Path to the north",
+        "ZONEDESC": "You can just barely see the entrance of the village",
+        "NORTH": True,
+        "SOUTH": True,
+        "EAST": False,
+        "WEST": False
+    },
+    "04": {
+        "ZONENAME": "Village",
+        "ZONEDESC": "You made it!",
+        "NORTH": False,
+        "SOUTH": True,
+        "EAST": False,
+        "WEST": False
+    },
+    "-10": {
+        "ZONENAME": "Path to the west",
+        "ZONEDESC": "There seems to be nothing down this way, and you don't feel eager going down there",
+        "NORTH": False,
+        "SOUTH": False,
+        "EAST": True,
+        "WEST": False
+    },
+}
